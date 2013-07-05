56615b90846d742932dafba9812531277e00c3ac
JohnLim <johnlim.dingwei@gmail.com>
2013-07-05 21:38:29 +0800

    Pulled content from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var fileOutput = fs.readFileSyne("input.html");
+    var buffer = new Buffer(fileOutput);
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
